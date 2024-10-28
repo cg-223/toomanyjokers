660d31468667ac411ca73b9abe45173f3d6bda63
cg-223 <cg223celeste@gmail.com>
2024-10-28 10:24:07 -0400

    add text suggesting TMJ existence to collection panels

--- steamodded.lua
+++ steamodded.lua
@@ -59,3 +59,23 @@ G.draw = function(...)
     end
     olddraw(...)
 end
+
+local oldcuib = create_UIBox_generic_options
+create_UIBox_generic_options = function(arg1, ...) --inserts the text into most collection pages without needing to hook each individual function
+    if arg1.back_func == "your_collection" and arg1.contents[1].n == 4 then
+        table.insert(arg1.contents, {
+            n = 4,
+            config = { align = "cm", minh = 1 },
+            nodes = {
+                {
+                    n = G.UIT.R,
+                    config = { align = "cm" },
+                    nodes = {
+                        { n = G.UIT.C, config = { align = "cl", minw = 5 }, nodes = { { n = G.UIT.T, config = { text = "Press T to access Too Many Jokers", colour = G.C.WHITE, shadow = true, scale = 0.5 } } } }
+                    }
+                }
+            }
+        })
+    end
+    return oldcuib(arg1, ...)
+end
