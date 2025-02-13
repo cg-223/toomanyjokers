30569605569e27cc5607bb7e2cf110fc02417235
cg223 <cg223celeste@gmail.com>
2025-02-13 08:46:00 -0500

    don't open TMJ during config

--- TMJ/defineConfig.lua
+++ TMJ/defineConfig.lua
@@ -5,13 +5,6 @@ local s = TMJ.FUNCS.commaSplit(config)
 TMJ.config.rows, TMJ.config.columns, TMJ.config.size = s[1] or "5", s[2] or "3", s[3] or "0.75"
 
 SMODS.current_mod.config_tab = function()
-    G.E_MANAGER:add_event(Event{
-        func = function()
-            G.FUNCS.TMJUIBOX()
-            return true
-        end,
-        delay = 0.3,
-    })
     return {
         n = G.UIT.ROOT,
         config = { align = "cm", minh = G.ROOM.T.h * 0.25, padding = 0.0, r = 0.1, colour = G.C.GREY },
