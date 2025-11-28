4ee867bff1a6c694d45e9f58e5bc25533091a68c
cg223 <cg223celeste@gmail.com>
2025-11-28 06:05:50 -0500

    fix crash when debugplus isnt installed

--- TMJ/utils.lua
+++ TMJ/utils.lua
@@ -94,8 +94,8 @@ function collect(iter)
 end
 
 function is_debugplus_console_open()
-    local dbp = require("debugplus.console")
-    if dbp and dbp.isConsoleFocused and dbp.isConsoleFocused() then
+    local succ, dbp = pcall(require, "debugplus.console")
+    if succ and dbp and dbp.isConsoleFocused and dbp.isConsoleFocused() then
         return true
     end
 end
